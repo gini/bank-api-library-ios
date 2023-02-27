1ac38f293e423a13997888d0b65957d16d6dd62a
Team Mobile Schorsch <team-mobile@gini.net>
2023-02-27 15:15:48 +0000

    Release version 1.5.0

--- Tests/GiniBankAPILibraryTests/DocumentServiceTests.swift
+++ Tests/GiniBankAPILibraryTests/DocumentServiceTests.swift
@@ -179,7 +179,7 @@ final class DocumentServicesTests: XCTestCase {
                                     osName: UIDevice.current.systemName,
                                     osVersion: UIDevice.current.systemVersion,
                                     captureSdkVersion: "Not available",
-                                    apiLibVersion: "3.0.0-beta04",
+                                    apiLibVersion: "1.5.0",
                                     description: "Error logging test",
                                     documentId: "1234",
                                     originalRequestId: "5678")
